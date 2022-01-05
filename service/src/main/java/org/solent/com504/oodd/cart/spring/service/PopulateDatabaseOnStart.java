ddb2868313574a865ae9bc52aef168f4f4f3393c
Titus777 <4mahet92@solent.ac.uk>
2022-01-05 18:49:26 +0000

    populate

--- service/src/main/java/org/solent/com504/oodd/cart/spring/service/PopulateDatabaseOnStart.java
+++ service/src/main/java/org/solent/com504/oodd/cart/spring/service/PopulateDatabaseOnStart.java
@@ -5,46 +5,46 @@
  */
 package org.solent.com504.oodd.cart.spring.service;
 
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.solent.com504.oodd.cart.dao.impl.ShoppingItemCatalogRepository;
 import org.solent.com504.oodd.cart.dao.impl.UserRepository;
+import org.solent.com504.oodd.cart.model.dto.ShoppingItem;
 import org.solent.com504.oodd.cart.model.dto.User;
 import org.solent.com504.oodd.cart.model.dto.UserRole;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 /**
  *
  * @author cgallen
  */
 @Component
 public class PopulateDatabaseOnStart {
-
     private static final Logger LOG = LogManager.getLogger(PopulateDatabaseOnStart.class);
-
     private static final String DEFAULT_ADMIN_USERNAME = "globaladmin";
     private static final String DEFAULT_ADMIN_PASSWORD = "globaladmin";
-
     private static final String DEFAULT_USER_PASSWORD = "user1234";
     private static final String DEFAULT_USER_USERNAME = "user1234";
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ShoppingItemCatalogRepository shoppingItemCatalogRepository;
+
     @PostConstruct
     public void initDatabase() {
         LOG.debug("initialising database with startup values");
-
         // initialising admin and normal user if dont exist
         User adminUser = new User();
         adminUser.setUsername(DEFAULT_ADMIN_USERNAME);
         adminUser.setFirstName("default administrator");
         adminUser.setPassword(DEFAULT_ADMIN_PASSWORD);
         adminUser.setUserRole(UserRole.ADMINISTRATOR);
-
         List<User> users = userRepository.findByUsername(DEFAULT_ADMIN_USERNAME);
         if (users.isEmpty()) {
             userRepository.save(adminUser);
@@ -52,13 +52,11 @@ public class PopulateDatabaseOnStart {
         } else {
             LOG.info("default admin user already exists. Not creating new :" + adminUser);
         }
-
         User defaultUser = new User();
         defaultUser.setUsername(DEFAULT_USER_USERNAME);
         defaultUser.setFirstName("default user");
         defaultUser.setPassword(DEFAULT_USER_PASSWORD);
         defaultUser.setUserRole(UserRole.CUSTOMER);
-
         users = userRepository.findByUsername(DEFAULT_USER_USERNAME);
         if (users.isEmpty()) {
             userRepository.save(defaultUser);
@@ -67,6 +65,17 @@ public class PopulateDatabaseOnStart {
             LOG.info("defaultuser already exists. Not creating new :" + defaultUser);
         }
 
+        List<ShoppingItem> itemlist = Arrays.asList(
+        new ShoppingItem("house",  20000.0 ),
+        new ShoppingItem("hen", 5.00),
+        new ShoppingItem("car", 5000.0),
+        new ShoppingItem("pet alligator", 65.0));
+
+        for (ShoppingItem item:itemlist){
+            shoppingItemCatalogRepository.save(item);
+        }
+
         LOG.debug("database initialised");
     }
+
 }
